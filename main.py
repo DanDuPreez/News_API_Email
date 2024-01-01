2e75da20fe942f3dfef90e2cfb6c67605fdae98d
Daniel du Preez <dandupreez@legiondev.co.za>
2024-01-01 16:00:52 +0200

    Send an email with titles and descriptions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
+from send_email import send_email
 
 api_key = 'c0085be4acde41e787412d941d3bcfe7'
-url = 'https://newsapi.org/v2/everything?q=tesla&'\
-      'from=2023-12-01&sortBy=publishedAt&apiKey='\
+url = 'https://newsapi.org/v2/everything?q=tesla&' \
+      'from=2023-12-01&sortBy=publishedAt&apiKey=' \
       'c0085be4acde41e787412d941d3bcfe7'
 
 # Make a request
@@ -12,6 +13,9 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article title and descriptions
+body = ""
 for article in content['articles']:
-      print(article['title'])
-      print(article['descriptions'])
+    body = body + article['title'] + '\n' + article['description'] + 2*'\n'
+
+body = body.encode('utf-8')
+send_email(message=body)
